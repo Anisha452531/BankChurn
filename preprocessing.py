1ed43b91b8cfe6729f1cebe1ad13fa615c21d495
Madhanalekha <madhanalekha203@gmail.com>
2025-05-05 17:18:28 +0530

    Updated content in BankChurn submodule

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,26 @@
+#Loading of DataSet
+import pandas as pd
+df = pd.read_csv("C:/Users/madha/OneDrive/Desktop/NM_ChurnPridiction/BankChurn/Churn_Modelling.csv")
+print(df.head())
+print("Shape of the dataset:",df.shape)
+print(df.info())
+#Data Preprocessing
+#1. No Missing Values
+print("Missing Values:\n", df.isnull().sum())
+#2. Remove Duplicates
+df = df.drop_duplicates()
+#3. Drop Irrelevant Columns
+if 'CustomerId' in df.columns:
+    df.drop('CustomerId', axis=1, inplace=True)
+if 'RowNumber' in df.columns:
+    df.drop('RowNumber', axis=1, inplace=True)
+print(df.head())
 
+#4. Encoding Categorical Features
+from sklearn.preprocessing import LabelEncoder
+le_gender = LabelEncoder()
+df['Gender'] = le_gender.fit_transform(df['Gender'])
+le_geo = LabelEncoder()
+df['Geography'] = le_geo.fit_transform(df['Geography'])
+print (df.head())
+#5. Scaling and Standardization
